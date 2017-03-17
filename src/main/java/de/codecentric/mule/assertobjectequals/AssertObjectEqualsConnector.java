118185e957405d9113e7bbfdd983b8fc6d16bca3
butenuth <butenuth@RBUTENUTH.codecentric.local>
2017-03-17 18:00:20 +0100

    Configuration experiments

--- src/main/java/de/codecentric/mule/assertobjectequals/AssertObjectEqualsConnector.java
+++ src/main/java/de/codecentric/mule/assertobjectequals/AssertObjectEqualsConnector.java
@@ -2,68 +2,50 @@ package de.codecentric.mule.assertobjectequals;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.mule.api.annotations.Category;
+import org.mule.api.annotations.Config;
+import org.mule.api.annotations.Configurable;
 import org.mule.api.annotations.Connector;
 import org.mule.api.annotations.Processor;
+import org.mule.api.annotations.components.Configuration;
+import org.mule.api.annotations.display.FriendlyName;
+import org.mule.api.annotations.display.Placement;
+import org.mule.api.annotations.display.Summary;
 import org.mule.api.annotations.param.Default;
+import org.mule.extension.annotations.param.Optional;
 
 @Connector(name = "assert-object-equals", friendlyName = "Assert Object Equals", description = "Compares two structures (Map/List based)")
 @Category(name = "org.mule.tooling.category.munit", description = "MUnit")
 public class AssertObjectEqualsConnector {
-	private ObjectComparator comparator = null;//new ObjectComparator();
+    private ObjectComparator comparator = null;// new ObjectComparator();
 
-	/**
-	* String represantation of the path pattern. String must contain
-	* zero to <code>n</code> parts. The parts must comply with the following syntax:
-	* <ul>
-	* <li><code>?</code>: Wildcard one, matches one element in a path</li>
-	* <li><code>*</code>: Wildcard any, matches zero to <code>n</code> elements in a path</li>
-	* <li><code>[#]</code>: List wildcard, matches a list entry with any index</li>
-	* <li><code>[0]</code>: Matches a list entry with the given number. 0 or positive numbers: Count from beginning, negative number: Cound from end (-1 is last element)</li>
-	* <li><code>['.*']</code>: Matches a map entry where the key must match the given regular expression. If you need a ' in the expression, just write ''. The example '.*' matches all keys.</li>
-	* </ul>
-	 */
-	//	@Configurable
-	//	@Placement(group = "Objects")
-	//	@FriendlyName("Path pattern")
-	private Map<String, List<PathOption>> pathObjections;
+    // @Configurable
+    // @Placement(group = "XML")
+    // @FriendlyName("Todo...")
+    // private String someXmlStuff;
 
-	//	@Configurable
-	//	@Placement(group = "XML")
-	//	@FriendlyName("Todo...")
-	private String someXmlStuff;
-
-	/**
-	 * Compare two objects. Drill down into {@link Map} and {@link List}, use
-	 * {@link Object#equals(Object)} for all other classes.
-	 *
-	 * @param expected
-	 *            The expected value.
-	 * @param actual
-	 *            The actual value.
-	 * @return <code>actual</code>
-	 */
-	@Processor(friendlyName = "Compare objects")
-	public Object equals(Object expected, @Default("#[payload]") Object actual) {
-		//comparator.compare(new PathState(new Path(), true, true), expected, actual);
-		// java.lang.AssertionError
-		return actual;
-	}
-
-	public Map<String, List<PathOption>> getPathObjections() {
-		return pathObjections;
-	}
-
-	public void setPathObjections(Map<String, List<PathOption>> aPathObjections) {
-		pathObjections = aPathObjections;
-	}
-
-	public String getSomeXmlStuff() {
-		return someXmlStuff;
-	}
-
-	public void setSomeXmlStuff(String aSomeXmlStuff) {
-		someXmlStuff = aSomeXmlStuff;
-	}
+    /**
+     * Compare two objects. Drill down into {@link Map} and {@link List}, use
+     * {@link Object#equals(Object)} for all other classes.
+     *
+     * @param expected
+     *            The expected value.
+     * @param actual
+     *            The actual value.
+     * @return <code>actual</code>
+     */
+    @Processor(friendlyName = "Compare objects")
+    public Object equals(Object expected, //
+            @Default("#[payload]") Object actual,
+            @Optional
+            @FriendlyName("Path pattern")
+            Map<String, String> pathOtions
+            ) {
+        // comparator.compare(new PathState(new Path(), true, true), expected,
+        // actual);
+        // java.lang.AssertionError
+        return actual;
+    }
 }
